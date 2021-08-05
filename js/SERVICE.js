2d4dc304b57b067a60948cefa52da8fc4cffd811
Irene Syu <weiyunsyu@gmail.com>
2021-08-05 22:15:01 +0800

    create service other page

--- js/SERVICE.js
+++ js/SERVICE.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
         $('.service_left li').find('a').eq(2).toggleClass('on').parent().siblings().find('a').removeClass('on');
     })
     $('.SER_QnA').click(function() {
-        $('.service_left li').find('a').eq(4).toggleClass('on').parent().siblings().find('a').removeClass('on');
+        $('.service_left li').find('a').eq(3).toggleClass('on').parent().siblings().find('a').removeClass('on');
     })
     $('.SER_PRO').click(function() {
-        $('.service_left li').find('a').eq(3).toggleClass('on').parent().siblings().find('a').removeClass('on');
+        $('.service_left li').find('a').eq(4).toggleClass('on').parent().siblings().find('a').removeClass('on');
     })
 })
